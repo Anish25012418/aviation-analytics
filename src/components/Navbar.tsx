801617770570b73787e0966f69ed1badd9d79b55
anish <anishdangi.me@gmail.com>
2025-06-12 23:55:35 +0545

    feat: redirect to home page on logo click.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
+import {useNavigate} from "react-router-dom";
+
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="h-full flex flex-col shadow-sm">
       <div className="py-2 flex justify-between items-center">
-        <img src="/logo-light.png" alt="logo" className="w-70 lg:w-90" />
+        <img src="/logo-light.png" alt="logo" className="w-70 lg:w-90 cursor-pointer" onClick={() => navigate("/")}/>
       </div>
     </nav>
   );
